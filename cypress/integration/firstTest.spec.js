a1d60c7f9dc5c0cf1a9c82554d6c2e748ab1fc82
TonyHomsi <tonyhomsi87@gmail.com>
2021-01-21 10:05:22 +0100

    Verify correct request and response

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -5,8 +5,26 @@ describe('Test with backend', ()=> {
         cy.loginToApplication()
     })
 
-    it.only('should log in', () =>{
-        cy.log('Yes we logged in!')
+    it.only('VERIFY CORRECT REQUEST AMD RESPONSE', () =>{
+
+        cy.server()
+        cy.route('POST', '**/articles').as('postArticles')
+
+
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('This is a title')
+        cy.get('[formcontrolname="description"]').type('This is a description')
+        cy.get('[formcontrolname="body"]').type('This is a body of the Articless')
+        cy.contains('Publish Article').click()
+
+        cy.wait('@postArticles')
+        cy.get('@postArticles').then( xhr =>{
+            console.log(xhr)
+            expect(xhr.status).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is a body of the Articless')
+            expect(xhr.response.body.article.description).to.equal('This is a description')
+        })
+
     })
 
 })
