a620641314077651fb9c439da035ca22982173b9
TonyHomsi <tonyhomsi87@gmail.com>
2021-01-22 16:41:56 +0100

    environment variable

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,7 +15,25 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+
+
+const fs = require('fs-extra')
+const path = require('path')
+
+function getConfigurationByFile (file) {
+  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+  if(!fs.existsSync(pathToConfigFile)){
+    return{};
+  }
+
+  return fs.readJson(pathToConfigFile)
+}
+
 module.exports = (on, config) => {
+
+  const file = config.env.configFile
+
+  return getConfigurationByFile(file)
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
